5f45a5b27174533a85f8126402ba87ff5905d410
Mauricio Flores <contact@maurojflores.com>
2018-12-21 12:11:07 -0600

    Added responsive styles to nav and splash

--- src/js/script.js
+++ src/js/script.js
@@ -23,11 +23,16 @@ function () {
 
     this.stickyClass = options.stickyClass; // track whether or not the nav has is in the sticky state
 
-    this.isSticky = false; // bind context of all methods to current instance
+    this.isSticky = false;
+    this.$menuButton = document.querySelector(options.menuButtonSelector);
+    this.$menu = document.querySelector(options.menuSelector);
+    this.openMenuClass = options.openMenuClass; // bind context of all methods to current instance
 
     this.init = this.init.bind(this);
     this.bindEvents = this.bindEvents.bind(this);
-    this.stick = this.stick.bind(this); // run all initial functionality
+    this.stick = this.stick.bind(this);
+    this.toggleMenu = this.toggleMenu.bind(this);
+    this.disableStick = this.disableStick.bind(this); // run all initial functionality
 
     this.init();
   }
@@ -42,24 +47,39 @@ function () {
     value: function bindEvents() {
       // set up listener for scroll event
       window.addEventListener('scroll', this.stick);
+      window.addEventListener('resize', this.disableStick);
+      this.$menuButton.addEventListener('click', this.toggleMenu);
     }
   }, {
     key: "stick",
     value: function stick() {
+      if (window.innerWidth < 960) return false;
+
       if (window.scrollY > this.heightAboveNav && this.isSticky) {
         // do nothing if nav is already in sticky state, and user is scrolling past sticking point
         return;
       } else if (window.scrollY > this.heightAboveNav && !this.isSticky) {
         // user is scrolling past sticking point while nav is in initial state
-        this.$nav.classList.add(this.stickyClass); // this.$mainWrap.style.marginTop = this.$nav.offsetHeight + 'px';
-
+        this.$nav.classList.add(this.stickyClass);
         this.isSticky = true;
       } else {
         // set nav back to initial state if scrolling back to sticking point
-        this.$nav.classList.remove(this.stickyClass); // this.$mainWrap.style.marginTop = 0;
-
+        this.$nav.classList.remove(this.stickyClass);
         this.isSticky = false;
       }
+    }
+  }, {
+    key: "disableStick",
+    value: function disableStick() {
+      if (window.innerWidth < 960) {
+        this.$nav.classList.remove(this.stickyClass);
+        this.isSticky = false;
+      }
+    }
+  }, {
+    key: "toggleMenu",
+    value: function toggleMenu() {
+      if (this.$menu.classList.contains(this.openMenuClass)) this.$menu.classList.remove(this.openMenuClass);else this.$menu.classList.add(this.openMenuClass);
     }
   }]);
 
@@ -71,7 +91,10 @@ window.addEventListener('load', function () {
   var stickyNav = new StickyNav({
     navSelector: '.main-nav',
     mainWrapSelector: '.page-wrap',
-    stickyClass: 'sticky'
+    stickyClass: 'sticky',
+    menuButtonSelector: '.menu-button',
+    menuSelector: '.main-nav-links',
+    openMenuClass: 'open'
   });
 });
 var test = 'Testing!';
