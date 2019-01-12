acbdd4f797859039fc11046b448bb691a976516f
Mauricio Flores <contact@maurojflores.com>
2019-01-12 11:24:58 -0600

    Added loading screen, updated readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Glide Theme
 
 A multi-purpose theme of HTML, CSS, and JS templates
+
+[See live demo](http://ui.maurojflores.com/glide/dist/)
--- src/js/script.js
+++ src/js/script.js
@@ -3844,6 +3844,70 @@ window.addEventListener('load', function () {
   });
 });
 
+var LoadingScreen =
+/*#__PURE__*/
+function () {
+  function LoadingScreen(selector) {
+    _classCallCheck(this, LoadingScreen);
+
+    // the element wrapping the loading screen content
+    this.$body = document.querySelector('body');
+    this.$loadingScreen = this.$body.querySelector(selector); // bind all methods' contexts to created instance
+
+    this.init = this.init.bind(this);
+    this.simulateLoading = this.simulateLoading.bind(this);
+    this.fadeLoadingScreen = this.fadeLoadingScreen.bind(this); // run initial functionality
+
+    this.init();
+  }
+
+  _createClass(LoadingScreen, [{
+    key: "init",
+    value: function init() {
+      // shrink body height to screen size while loading screen is visible
+      this.$body.style.height = window.height + 'px';
+      this.$body.style.overflow = 'hidden'; // do not call this method on a real site; this method is just to simulate
+      // loading time on this demo
+      // this.simulateLoading(); // delete/comment this line on your site
+      // un-comment the line below on your site
+
+      this.fadeLoadingScreen();
+    }
+    /**
+     * This method is only used to simulate loading time on this demo; do not run it on your site
+     */
+
+  }, {
+    key: "simulateLoading",
+    value: function simulateLoading() {
+      setTimeout(this.fadeLoadingScreen, 4000);
+    }
+    /**
+     * Add `fulfilled` class to loading screen to trigger the css fadeOut animation
+     */
+
+  }, {
+    key: "fadeLoadingScreen",
+    value: function fadeLoadingScreen() {
+      var _this2 = this;
+
+      this.$loadingScreen.classList.add('fulfilled');
+      this.$body.style.height = 'auto';
+      this.$body.style.overflow = 'initial'; // hide element completely after 1.5 seconds
+
+      setTimeout(function () {
+        _this2.$loadingScreen.style.display = 'none';
+      }, 1500);
+    }
+  }]);
+
+  return LoadingScreen;
+}();
+
+window.addEventListener('load', function () {
+  var loadingScreen = new LoadingScreen('#loading-screen');
+});
+
 var ScrollDownButton =
 /*#__PURE__*/
 function () {
